218333151e1e2e925a55ea3294497ec751b12457
Mikhail <mikle0240@gmail.com>
2023-08-28 15:12:43 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketCost = 13_676;
+        int rub = 20;
+        int bonus = (ticketCost / rub);
+        System.out.println("Количество начисленных бонусов: " + bonus);
+
+    }
+}
